b010990441dc17a4169239faf09074f3c6f22290
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-03-10 01:30:22 +0100

    feat: añade primera version grupos aleatorios por preferencias

--- grupo/urls.py
+++ grupo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import crear_grupo, crear_grupo_admin, obtener_miembros_de_grupos
+from .views import crear_grupo, crear_grupo_admin, obtener_miembros_de_grupos,ag
 
 urlpatterns = [
     path('crear_grupo/', crear_grupo, name='crear_grupo'),
     path('crear_grupo/<str:ciclo>', crear_grupo_admin, name='crear_grupo_admin'),
-    path('miembros', obtener_miembros_de_grupos, name='obtener_miembros_de_grupos')
+    path('miembros', obtener_miembros_de_grupos, name='obtener_miembros_de_grupos'),
+    path('prueba', ag, name='ag')
 ]
