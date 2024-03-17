6bcd22e155e2fb6eb0533d462f56fb5a946454be
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-03-17 21:35:38 +0100

    feat: ruta de carpetas en el listado

--- drive/urls.py
+++ drive/urls.py
@@ -3,12 +3,10 @@ from .views import subir_archivo, listar_archivos, crear_carpeta
 
 
 urlpatterns = [
-    path('subir', subir_archivo, name='subir_archivo'),
+    path('subir/', subir_archivo, name='subir_archivo'),
     path('subir/<int:folder_id>', subir_archivo, name='subir_archivo_en_carpeta'),
-    path('listar', listar_archivos, name='listar_archivos'),
+    path('listar/', listar_archivos, name='listar_archivos'),
     path('listar/<int:folder_id>', listar_archivos, name='listar_archivos_en_carpeta'),
     path('crear_carpeta', crear_carpeta, name='crear_carpeta'),
     path('crear_carpeta/<int:folder_id>', crear_carpeta, name='crear_carpeta_en_carpeta'),
-
-
 ]
