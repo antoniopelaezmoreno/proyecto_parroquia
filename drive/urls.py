5148e7f3a1129be23707a005a06c4a3387175230
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-03-17 20:39:30 +0100

    feat: añade funcionalidad de carpetas al drive propio

--- drive/urls.py
+++ drive/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import subir_archivo, listar_archivos
+from .views import subir_archivo, listar_archivos, crear_carpeta
 
 
 urlpatterns = [
     path('subir', subir_archivo, name='subir_archivo'),
-    path('listar', listar_archivos, name='listar_archivos')
+    path('subir/<int:folder_id>', subir_archivo, name='subir_archivo_en_carpeta'),
+    path('listar', listar_archivos, name='listar_archivos'),
+    path('listar/<int:folder_id>', listar_archivos, name='listar_archivos_en_carpeta'),
+    path('crear_carpeta', crear_carpeta, name='crear_carpeta'),
+    path('crear_carpeta/<int:folder_id>', crear_carpeta, name='crear_carpeta_en_carpeta'),
 
 
 ]
