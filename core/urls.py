469a7facb466bb5a81b8acb0eddb4748751d4002
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-03-25 13:37:12 +0100

    feat: mejora frontend coord

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, c403, c404
+from .views import index, c403, c404, error
 
 urlpatterns = [
     path('', index, name='index'),
     path('403', c403, name='c403'),
     path('404', c404, name='c404'),
+    path('error', error, name='error'),
 
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, redirect
 from grupo.models import Grupo
+from datetime import date
+from sesion.models import Sesion
 
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
         if request.user.is_coord:
+            proxima_sesion = Sesion.objects.filter(ciclo = request.user.ciclo).filter(fecha__gte=date.today()).order_by('fecha').first()
             grupos = Grupo.objects.filter(ciclo=request.user.ciclo)
-            print("...............",grupos)
-            return render(request, 'index.html', {'grupos': grupos})
+            return render(request, 'index/index_coord.html', {'grupos': grupos, 'proxima_sesion': proxima_sesion})
+        elif request.user.is_superuser:
+            return render(request, 'index/index_admin.html')
         else:
-            return render(request, 'index.html')
-    return render(request, 'index.html')
+            return render(request, 'index/index_cat.html')
+    else:
+        return render(request, 'index/index.html')
 
 def c403(request):
     return render(request, '403.html')
 
 def c404(request):
     return render(request, '404.html')
+
+def error(request, mensaje):
+    return render(request, 'error.html', {'mensaje': mensaje})
