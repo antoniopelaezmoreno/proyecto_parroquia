4d2830ca2ea4385904c99afbb9215173d1657190
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-03-30 18:29:28 +0100

    feat: asociar archivos a sesiones

--- sesion/forms.py
+++ sesion/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from .models import Sesion
+from drive.models import File
 
 class SesionForm(forms.ModelForm):
+    files = forms.ModelMultipleChoiceField(queryset=File.objects.all(), required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['files'].label_from_instance = lambda obj: obj.name
+
     class Meta:
         model = Sesion
-        fields = ['titulo', 'descripcion', 'fecha']
+        fields = ['titulo', 'descripcion', 'fecha', 'files']
         widgets = {
-                'fecha': forms.DateInput(attrs={'type': 'date'})
-            }
+            'fecha': forms.DateInput(attrs={'type': 'date'})
+        }
