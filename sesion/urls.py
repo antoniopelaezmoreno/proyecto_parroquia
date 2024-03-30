88a9321a0d0d35258eb519be480667d3e7612143
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-03-30 18:39:38 +0100

    feat: añade editar sesion

--- sesion/urls.py
+++ sesion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import crear_sesion, listar_sesiones, pasar_lista,tabla_asistencias_grupo, tabla_asitencias_coord, tabla_asistencias_admin
+from .views import crear_sesion, listar_sesiones, pasar_lista,tabla_asistencias_grupo, tabla_asitencias_coord, tabla_asistencias_admin, editar_sesion
 
 urlpatterns = [
     path('crear', crear_sesion, name='crear_sesion'),
     path('listar', listar_sesiones, name='listar_sesiones'),
+    path('editar/<int:sesionId>', editar_sesion, name='editar_sesion'),
     path('pasar_lista/<int:sesionid>', pasar_lista, name='pasar_lista'),
     path('tabla_asistencia_grupo', tabla_asistencias_grupo, name='tabla_asistencias_grupo'),
     path('tabla_asistencia_coord', tabla_asitencias_coord, name='tabla_asistencias_coord'),
