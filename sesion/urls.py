9a694c4c71dd62ee3489f92bcd7020eae9f50a11
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-03-30 19:09:55 +0100

    feat: editar y eliminar grupos y sesiones

--- sesion/urls.py
+++ sesion/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import crear_sesion, listar_sesiones, pasar_lista,tabla_asistencias_grupo, tabla_asitencias_coord, tabla_asistencias_admin, editar_sesion
+from .views import crear_sesion, listar_sesiones, pasar_lista,tabla_asistencias_grupo, tabla_asitencias_coord, tabla_asistencias_admin, editar_sesion, eliminar_sesion
 
 urlpatterns = [
     path('crear', crear_sesion, name='crear_sesion'),
     path('listar', listar_sesiones, name='listar_sesiones'),
     path('editar/<int:sesionId>', editar_sesion, name='editar_sesion'),
+    path('eliminar/<int:sesionId>', eliminar_sesion, name='eliminar_sesion'),
     path('pasar_lista/<int:sesionid>', pasar_lista, name='pasar_lista'),
     path('tabla_asistencia_grupo', tabla_asistencias_grupo, name='tabla_asistencias_grupo'),
     path('tabla_asistencia_coord', tabla_asitencias_coord, name='tabla_asistencias_coord'),
