3a4c818cdfbc538e4f10395f110db9ee5a28fee5
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-04-07 15:43:08 +0200

    feat: añade validaciones en creación de reservas

--- sala/views.py
+++ sala/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Sala, Reserva
 from custom_user.models import CustomUser
 from datetime import timedelta, date
@@ -12,13 +12,20 @@ def obtener_salas_disponibles(request):
         hora_inicio = request.POST.get('hora_inicio')
         hora_fin = request.POST.get('hora_fin')
 
-        salas_disponibles = Sala.objects.exclude(
+        todas_salas = Sala.objects.all()
+        salas_ocupadas = todas_salas.filter(
             reserva__fecha=fecha,
             reserva__hora_inicio__lt=hora_fin,
             reserva__hora_fin__gt=hora_inicio
         )
-        print(salas_disponibles)
-        return render(request, 'salas_disponibles.html', {'salas_disponibles': salas_disponibles})
+        reservas_salas_ocupadas = Reserva.objects.filter(
+            sala__in=salas_ocupadas,
+            fecha=fecha,
+            hora_inicio__lt=hora_fin,
+            hora_fin__gt=hora_inicio
+        )
+        print(reservas_salas_ocupadas)
+        return render(request, 'salas_disponibles.html', {'todas_salas':todas_salas,'salas_ocupadas': salas_ocupadas, 'reservas_salas_ocupadas':reservas_salas_ocupadas})
 
     return render(request, 'salas_disponibles.html')
 
@@ -26,35 +33,48 @@ def obtener_salas_disponibles(request):
 
 @login_required
 def crear_reservas_por_defecto(request):
-    if request.method == 'POST':
-        usuario_id = request.POST.get('usuario')
-        sala_id = request.POST.get('sala')
-        dia_semana = int(request.POST.get('dia_semana'))
-        hora_inicio = request.POST.get('hora_inicio')
-        hora_fin = request.POST.get('hora_fin')
-        
-        # Obtener usuario y sala
-        usuario = CustomUser.objects.get(pk=usuario_id)
-        sala = Sala.objects.get(pk=sala_id)
-        
-        # Obtener la fecha actual
-        fecha_actual = date.today()
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            usuario_id = request.POST.get('usuario')
+            sala_id = request.POST.get('sala')
+            dia_semana = int(request.POST.get('dia_semana'))
+            hora_inicio = request.POST.get('hora_inicio')
+            hora_fin = request.POST.get('hora_fin')
+            
+            # Obtener usuario y sala
+            usuario = CustomUser.objects.get(pk=usuario_id)
+            sala = Sala.objects.get(pk=sala_id)
+            
+            # Obtener la fecha actual
+            fecha_actual = date.today()
 
-        # Calcular la fecha del próximo día de la semana elegido
-        dia_actual = fecha_actual.weekday()
-        dias_para_dia_elegido = (dia_semana - dia_actual + 7) % 7
-        proxima_fecha = fecha_actual + timedelta(days=dias_para_dia_elegido)
+            # Calcular la fecha del próximo día de la semana elegido
+            dia_actual = fecha_actual.weekday()
+            dias_para_dia_elegido = (dia_semana - dia_actual + 7) % 7
+            proxima_fecha = fecha_actual + timedelta(days=dias_para_dia_elegido)
 
-        # Calcular la fecha límite (30 de junio)
-        fecha_limite = date(fecha_actual.year, 6, 30)
+            # Calcular la fecha límite (30 de junio)
+            fecha_limite = date(fecha_actual.year, 6, 30)
 
-        # Crear reservas hasta la fecha límite (30 de junio) para cada viernes
-        while proxima_fecha <= fecha_limite:
-            reserva = Reserva.objects.create(usuario=usuario, sala=sala, fecha=proxima_fecha, hora_inicio=hora_inicio, hora_fin=hora_fin)
-            proxima_fecha += timedelta(weeks=1)
-        
-        return HttpResponse('Reservas creadas exitosamente.')  # O redirigir a una página de éxito
+            lista_reservas = []
+            # Crear reservas hasta la fecha límite (30 de junio) para cada viernes
+            while proxima_fecha <= fecha_limite:
+                reservas_exist = Reserva.objects.filter(sala=sala, fecha=proxima_fecha, hora_inicio__lt=hora_fin, hora_fin__gt=hora_inicio)
+                if reservas_exist.exists():
+                    print('Ya existe una reserva en el plazo seleccionado. La fecha ocupada es: ', reservas_exist.first().fecha)
+                    lista_reservas.clear()
+                    return HttpResponse('Ya existe una reserva en el plazo seleccionado. La fecha ocupada es: ', reservas_exist.first().fecha)
+                
+                reserva = Reserva(usuario=usuario, sala=sala, fecha=proxima_fecha, hora_inicio=hora_inicio, hora_fin=hora_fin)
+                lista_reservas.append(reserva)
+                proxima_fecha += timedelta(weeks=1)
 
-    salas = Sala.objects.all()
-    usuarios = CustomUser.objects.filter(is_coord=True)
-    return render(request, 'crear_reservas.html', {'salas': salas, 'usuarios': usuarios})
+            Reserva.objects.bulk_create(lista_reservas)
+            
+            return HttpResponse('Reservas creadas exitosamente.')  # O redirigir a una página de éxito
+
+        salas = Sala.objects.all()
+        usuarios = CustomUser.objects.filter(is_coord=True)
+        return render(request, 'crear_reservas.html', {'salas': salas, 'usuarios': usuarios})
+    else:
+        return redirect('/403')
