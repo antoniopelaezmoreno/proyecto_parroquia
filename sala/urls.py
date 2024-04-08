4b19114812d9a95df5698c9ec4ea052d9011dda3
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-04-08 16:09:29 +0200

    feat: mis reservas

--- sala/urls.py
+++ sala/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import obtener_salas_disponibles, crear_reservas_por_defecto, reservar_sala
+from .views import obtener_salas_disponibles, crear_reservas_por_defecto, reservar_sala, mis_reservas
 
 urlpatterns = [
-    path('obtener_libres/', obtener_salas_disponibles, name='obtener_libres'),
+    path('', obtener_salas_disponibles, name='lista_salas'),
     path('crear_reservas/', crear_reservas_por_defecto, name='crear_reservas'),
     path('reservar/', reservar_sala, name='reservar'),
+    path('mis_reservas/', mis_reservas, name='mis_reservas'),
 
 ]
