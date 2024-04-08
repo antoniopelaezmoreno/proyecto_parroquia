95e4f664cf856a61b5bb35245a0ed4e176b7bef3
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-04-08 13:35:32 +0200

    feat: añade crear reserva individual

--- sala/urls.py
+++ sala/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import obtener_salas_disponibles, crear_reservas_por_defecto
+from .views import obtener_salas_disponibles, crear_reservas_por_defecto, reservar_sala
 
 urlpatterns = [
     path('obtener_libres/', obtener_salas_disponibles, name='obtener_libres'),
     path('crear_reservas/', crear_reservas_por_defecto, name='crear_reservas'),
+    path('reservar/', reservar_sala, name='reservar'),
+
 ]
