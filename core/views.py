be4bffd2be9ffcb9fbd3085d9ed735fdd5ddfdc0
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-04-10 23:22:43 +0200

    feat: arreglos frontend coordinador

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from grupo.models import Grupo
 from datetime import date
 from sesion.models import Sesion
 from notificacion.models import Notificacion
+from catecumeno.models import Catecumeno
 
 # Create your views here.
 def index(request):
@@ -10,12 +11,13 @@ def index(request):
         notificaciones = Notificacion.objects.filter(destinatario=request.user, visto=False)
         if request.user.is_coord:
             proxima_sesion = Sesion.objects.filter(ciclo = request.user.ciclo).filter(fecha__gte=date.today()).order_by('fecha').first()
+            num_catecumenos = Catecumeno.objects.filter(ciclo=request.user.ciclo).count()
             if proxima_sesion:
                 archivos_sesion = proxima_sesion.files.all()
             else:
                 archivos_sesion = None
             grupos = Grupo.objects.filter(ciclo=request.user.ciclo)
-            return render(request, 'index/index_coord.html', {'grupos': grupos, 'proxima_sesion': proxima_sesion, 'archivos_sesion': archivos_sesion,'notificaciones': notificaciones})
+            return render(request, 'index/index_coord.html', {'grupos': grupos, 'proxima_sesion': proxima_sesion, 'archivos_sesion': archivos_sesion,'notificaciones': notificaciones, 'num_catecumenos': num_catecumenos})
         elif request.user.is_superuser:
             return render(request, 'index/index_admin.html')
         else:
