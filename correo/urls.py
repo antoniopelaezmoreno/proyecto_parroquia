0ab87d14adfbd86eac5470f64873f377e9f5d271
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-04-12 22:38:52 +0200

    feat: añade funcion enviar mensaje

--- correo/urls.py
+++ correo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import bandeja_entrada_familias, bandeja_entrada_catequistas
+from .views import bandeja_entrada_familias, bandeja_entrada_catequistas, enviar_correo
 
 urlpatterns = [
     path('inbox_familias', bandeja_entrada_familias, name='bandeja_de_entrada_familias'),
     path('inbox_catequistas', bandeja_entrada_catequistas, name='bandeja_de_entrada_catequistas'),
+    path('enviar_correo', enviar_correo, name='enviar_correo'),
 ]
