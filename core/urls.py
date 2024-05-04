aa544f11ddbb4425bbadfa82a8cfeef242b23959
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-05-04 15:34:48 +0200

    feat: añade terminar curso

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index, c403, c404, error
+from .views import index, c403, c404, error, terminar_curso
 
 urlpatterns = [
     path('', index, name='index'),
     path('403', c403, name='c403'),
     path('404', c404, name='c404'),
     path('error', error, name='error'),
+    path('terminar_curso', terminar_curso, name='terminar_curso'),
 
 
 ]
