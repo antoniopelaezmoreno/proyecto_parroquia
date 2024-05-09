f48e78a3d6c791a2c8367ff4f4340d11bb38deec
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-05-09 20:40:42 +0200

    feat: mejora frontend en index coord y en el drive

--- drive/urls.py
+++ drive/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import subir_archivo, listar_archivos, crear_carpeta, eliminar_archivo, eliminar_carpeta, mover_archivo, mover_carpeta
+from .views import subir_archivo, listar_archivos, crear_carpeta, eliminar_archivo, eliminar_carpeta, mover_archivo, mover_carpeta, cambiar_nombre_carpeta
 
 
 urlpatterns = [
@@ -13,4 +13,5 @@ urlpatterns = [
     path('eliminar_carpeta/<int:folder_id>', eliminar_carpeta, name='eliminar_carpeta'),
     path('mover/<int:file_id>', mover_archivo, name='mover_archivo'),
     path('mover_carpeta/<int:folder_id>', mover_carpeta, name='mover_carpeta'),
+    path('cambiar_nombre_carpeta/<int:folder_id>', cambiar_nombre_carpeta, name='cambiar_nombre_carpeta'),
 ]
