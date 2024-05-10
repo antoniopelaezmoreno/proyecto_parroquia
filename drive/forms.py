9c5b5ef0b3bc9f4871d41b432ee0417dd06cd780
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-05-11 01:48:51 +0200

    fix: corrige mover carpeta para que no pueda moverse una carpeta a subcarpeta

--- drive/forms.py
+++ drive/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import File, Folder
+from django.db.models import Q
 
 class FileForm(forms.ModelForm):
     class Meta:
@@ -21,7 +22,13 @@ class MoveFolderForm(forms.ModelForm):
         current_folder = kwargs.pop('current_folder', None)
         super().__init__(*args, **kwargs)
         if current_folder:
-            self.fields['parent_folder'].queryset = Folder.objects.exclude(id=current_folder.id)
+            # Obtener todas las subcarpetas de la carpeta actual
+            subfolders = current_folder.get_descendants()
+            # Excluir las subcarpetas del queryset del campo parent_folder
+            self.fields['parent_folder'].queryset = Folder.objects.exclude(
+                Q(id__in=[subfolder.id for subfolder in subfolders]) |
+                Q(id=current_folder.id)
+            )
         self.fields['parent_folder'].empty_label = 'Principal'
 
     class Meta:
--- drive/models.py
+++ drive/models.py
@@ -7,8 +7,14 @@ class Folder(models.Model):
     name = models.CharField(max_length=30)
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     files = models.ManyToManyField('File', related_name='folders')
-    parent_folder = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    parent_folder = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subfolders')
 
+    def get_descendants(self):
+        descendants = [self]
+        for subfolder in self.subfolders.all():
+            descendants.extend(subfolder.get_descendants())
+        return descendants
+    
     def __str__(self):
         return self.name
 
@@ -64,11 +70,13 @@ class File(models.Model):
         return self.name
     
     def clean(self):
+        # Si el archivo ya existe, entonces se valida la extensión
         if self.file:
             try:
                 FileExtensionValidator(allowed_extensions=['pdf','jpeg','jpg','png','doc','docx','dot','dotx','docm','dotm','xls','xlsx','xlsm','xlsb','xltx','xltm','ppt','pptx','pot','potx','pptm','potm','mp4','mov','avi','mkv','txt'])(self.file)
             except ValidationError as e:
                 raise ValidationError({'file': ('Este tipo de archivo no está permitido')}) 
-        else:
+        # Si se está moviendo el archivo y no se selecciona un nuevo archivo, no se realiza ninguna validación
+        elif not self.parent_folder:
             raise ValidationError({'file': ('Debe subir un archivo.')})
         
