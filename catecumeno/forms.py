d757db5df9e5ccadc1e41b4da766e50309fa6391
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-05-13 01:01:09 +0200

    feat: mejora frontend mostrar datos de catecumeno

--- catecumeno/forms.py
+++ catecumeno/forms.py
@@ -8,3 +8,8 @@ class CatecumenoForm(forms.ModelForm):
         fields = ['nombre', 'apellidos', 'email', 'dni','telefono','ciclo', 'nombre_madre', 'apellidos_madre', 'email_madre', 'telefono_madre', 'nombre_padre', 'apellidos_padre', 'email_padre', 'telefono_padre', 'preferencias', 'foto']
 
 
+class CatecumenoEditForm(forms.ModelForm):
+    class Meta:
+        model = Catecumeno
+        fields = ['nombre', 'apellidos', 'email', 'dni','telefono','ciclo', 'nombre_madre', 'apellidos_madre', 'email_madre', 'telefono_madre', 'nombre_padre', 'apellidos_padre', 'email_padre', 'telefono_padre', 'foto']
+
