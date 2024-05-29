06e1ae0982cfd5aef28345341a8b578b53cf74a2
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-05-29 14:44:57 +0200

    feat: incluye formulario de registro en index

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, c403, c404, error, terminar_curso, panel_ciclos, panel_ultimas_ausencias, notificar_familias_ultima_ausencia, panel_ausencias_reiteradas, notificar_familias_ausencias_reiteradas
+from .views import index, c403, c404, error, terminar_curso, panel_ciclos, panel_ultimas_ausencias, notificar_familias_ultima_ausencia, panel_ausencias_reiteradas, notificar_familias_ausencias_reiteradas, pantalla_confirmacion_exito
 
 urlpatterns = [
     path('', index, name='index'),
@@ -12,6 +12,7 @@ urlpatterns = [
     path('notificar_familias_ultima_ausencia/<int:catecumeno_id>', notificar_familias_ultima_ausencia, name='notificar_familias_ultima_ausencia'),
     path('panel_ausencias_reiteradas', panel_ausencias_reiteradas, name='panel_ausencias_reiteradas'),
     path('notificar_familias_ausencias_reiteradas/<int:catecumeno_id>', notificar_familias_ausencias_reiteradas, name='notificar_familias_ausencias_reiteradas'),
+    path('pantalla_confirmacion_exito', pantalla_confirmacion_exito, name='pantalla_confirmacion_exito'),
 
 
 ]
