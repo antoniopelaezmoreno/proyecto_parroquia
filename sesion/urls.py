4d3357369b1c55357af4a57848be6b2916ceefd1
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-06-04 20:38:10 +0200

    feat: detalles sesion

--- sesion/urls.py
+++ sesion/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import crear_sesion, listar_sesiones, pasar_lista,tabla_asistencias_grupo, tabla_asitencias_coord, tabla_asistencias_admin, editar_sesion, eliminar_sesion
+from .views import crear_sesion, listar_sesiones, pasar_lista,tabla_asistencias_grupo, tabla_asitencias_coord, tabla_asistencias_admin, editar_sesion, eliminar_sesion, mostrar_sesion
 
 urlpatterns = [
     path('crear', crear_sesion, name='crear_sesion'),
@@ -9,7 +9,8 @@ urlpatterns = [
     path('pasar_lista/<int:sesionid>', pasar_lista, name='pasar_lista'),
     path('tabla_asistencia_grupo', tabla_asistencias_grupo, name='tabla_asistencias_grupo'),
     path('tabla_asistencia_coord', tabla_asitencias_coord, name='tabla_asistencias_coord'),
-    path('tabla_asistencia_admin/<str:ciclo>', tabla_asistencias_admin, name='tabla_asistencias_admin')
+    path('tabla_asistencia_admin/<str:ciclo>', tabla_asistencias_admin, name='tabla_asistencias_admin'),
+    path('<int:sesionId>', mostrar_sesion, name='mostrar_sesion'),
 
 
 ]
