885206104dc8ce56fdc815d65767ef92de15db8b
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-06-05 00:16:32 +0200

    feat: modifica frontend asignar catequistas

--- solicitud_catequista/urls.py
+++ solicitud_catequista/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import crear_solicitud_cateqista, asignar_catequistas
+from .views import crear_solicitud_cateqista, asignar_catequistas, eliminar_solicitud
 
 urlpatterns = [
     path('crear/', crear_solicitud_cateqista, name='crear_solicitud_catequista'),
     path('asignar_catequistas/', asignar_catequistas, name='asignar_catequistas'),
+    path('eliminar_solicitud/<int:solicitud_id>/', eliminar_solicitud, name='eliminar_solicitud'),
 
 ]
